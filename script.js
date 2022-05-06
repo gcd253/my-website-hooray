cd99d63436430a5c7ce66f5d440bbc7d979ea92d
Griffin Dooley <dooleygriffin@gmail.com>
2022-05-05 21:38:10 -0400

    Early sound board test

--- script.js
+++ script.js
@@ -2,3 +2,30 @@ const h2 = document.createElement("h2");
 h2.textContent = "This content added by JavaScript";
 
 document.querySelector("body").appendChild(h2);
+
+const sounds = ["running"]
+
+sounds.forEach(sound => {
+    const btn = document.createElement("button")
+    btn.classList.add("btn")
+
+    btn.innerText = sound
+
+    btn.addEventListener("click", () => {
+        stopSongs()
+
+        document.getElementById(sound).play()
+    })
+
+    document.getElementById("buttons").
+    appendChild(btn)
+})
+
+function stopSongs() {
+    sounds.forEach(sound => {
+        const song = document.getElementById(sound)
+
+        song.pause()
+        song.currentTime = 0;
+    })
+}
